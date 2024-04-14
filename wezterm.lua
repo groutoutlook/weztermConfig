083d09f1fb3622cbc5bbe16dba4edc21f1dbe152
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-04-15 01:18:48 +0700

    :sparkles: ft: remember the screen state

--- wezterm.lua
+++ wezterm.lua
@@ -232,7 +232,37 @@ mouse_bindings = {
 		mods = "NONE",
 	},
 }
-config.window_decorations = "NONE"
+local cache_dir = "$HOME/.cache/wezterm/"
+local window_size_cache_path = cache_dir .. "window_size_cache.txt"
+
+wezterm.on("gui-startup", function()
+	os.execute("mkdir " .. cache_dir)
+
+	local window_size_cache_file = io.open(window_size_cache_path, "r")
+	if window_size_cache_file ~= nil then
+		_, _, width, height = string.find(window_size_cache_file:read(), "(%d+),(%d+)")
+		mux.spawn_window({ width = tonumber(width), height = tonumber(height) })
+		window_size_cache_file:close()
+	else
+		local tab, pane, window = mux.spawn_window({})
+		window:gui_window():maximize()
+	end
+end)
+
+wezterm.on("window-resized", function(window, pane)
+	local window_size_cache_file = io.open(window_size_cache_path, "r")
+	if window_size_cache_file == nil then
+		local tab_size = pane:tab():get_size()
+		cols = tab_size["cols"]
+		rows = tab_size["rows"] + 2 -- Without adding the 2 here, the window doesn't maximize
+		contents = string.format("%d,%d", cols, rows)
+		window_size_cache_file = assert(io.open(window_size_cache_path, "w"))
+		window_size_cache_file:write(contents)
+		window_size_cache_file:close()
+	end
+end)
+
+-- config.window_decorations = "NONE"
 config.window_background_opacity = 0.2
 --- Default config settings
 config.scrollback_lines = 7000
